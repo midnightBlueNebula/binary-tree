47d25a5a5671c8b68d175c61c94aa859747752d6
Onur Kaya <46904292+varmchairDev@users.noreply.github.com>
2019-11-05 21:49:01 +0300

    Create node.rb

---
+++ node.rb
@@ -0,0 +1,72 @@
+class Node
+  attr_accessor :root, :left, :right
+
+  def initialize(root=nil,left=nil,right=nil)
+    @root = root
+    @left = left
+    @right = right
+  end
+
+  private
+  def complete_roots(num,arr)
+    min_root = num
+    while num.nil? == false
+      num = $roots[num] if num != nil
+      num = num[0] if num != nil
+      min_root = num if num != nil
+      p num
+    end
+    p "min_root: #{min_root}"
+    if min_root != arr[0]
+      $roots[min_root] = [arr[0],nil] 
+      min_root = $roots[min_root]
+      min_root = min_root[0]
+      p "new min_root: #{min_root}"
+      p $roots
+    end
+  end
+
+  def find_roots(arr,clear=true)
+    $roots = {} if clear
+    return arr[0] if arr.length == 1
+    return arr[1] if arr.length == 2
+    mid = arr[arr.length/2]
+    arr[arr.length/2] = nil
+    $roots[mid] = []
+    $roots[mid] << find_roots(arr.slice(0,arr.length/2),false)
+    $roots[mid] << find_roots(arr.slice(arr.length/2,arr.length),false)
+    return mid
+  end
+
+  public
+  def build_tree(arr=nil,first_call=true,root=nil)
+    if first_call && arr.kind_of?(Array)
+      arr.uniq!
+      arr.sort!
+      root = arr[arr.length/2]
+      find_roots(arr)
+      complete_roots(root,arr)
+      self.root = root
+      self.left = build_tree(nil,false,$roots[root][0])
+      self.right = build_tree(nil,false,$roots[root][1])
+    elsif arr.kind_of?(Array) == false && first_call
+      p "Error: Argument should be an Array"
+      return
+    end
+    if root.nil? == false && $roots[root] != nil
+      root = Node.new(root,$roots[root][0],$roots[root][1])
+      p root
+      root.left = build_tree(nil,false,root.left)
+      root.right = build_tree(nil,false,root.right)
+      return root
+    elsif root.nil? == false && $roots[root] == nil
+      return root
+    end
+  end
+end
+
+
+a = Node.new
+a.build_tree([1,2,3,4,5,6,7,8,9,10,11,12,13])
+a=a.left
+a=a.left
